9771af7dbf67b8187849bc5fc3f40fd706348805
Pat51438 <150244170+Pat51438@users.noreply.github.com>
2024-05-23 15:13:46 -0400

    reset score when dont click on object

---
+++ src/graphql/queries.ts
@@ -0,0 +1,88 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+import * as APITypes from "../API";
+type GeneratedQuery<InputType, OutputType> = string & {
+  __generatedQueryInput: InputType;
+  __generatedQueryOutput: OutputType;
+};
+
+export const getUserScore = /* GraphQL */ `query GetUserScore($id: ID!) {
+  getUserScore(id: $id) {
+    id
+    userId
+    name
+    score
+    createdAt
+    updatedAt
+    _version
+    _deleted
+    _lastChangedAt
+    __typename
+  }
+}
+` as GeneratedQuery<
+  APITypes.GetUserScoreQueryVariables,
+  APITypes.GetUserScoreQuery
+>;
+export const listUserScores = /* GraphQL */ `query ListUserScores(
+  $filter: ModelUserScoreFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUserScores(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      userId
+      name
+      score
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+    nextToken
+    startedAt
+    __typename
+  }
+}
+` as GeneratedQuery<
+  APITypes.ListUserScoresQueryVariables,
+  APITypes.ListUserScoresQuery
+>;
+export const syncUserScores = /* GraphQL */ `query SyncUserScores(
+  $filter: ModelUserScoreFilterInput
+  $limit: Int
+  $nextToken: String
+  $lastSync: AWSTimestamp
+) {
+  syncUserScores(
+    filter: $filter
+    limit: $limit
+    nextToken: $nextToken
+    lastSync: $lastSync
+  ) {
+    items {
+      id
+      userId
+      name
+      score
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      __typename
+    }
+    nextToken
+    startedAt
+    __typename
+  }
+}
+` as GeneratedQuery<
+  APITypes.SyncUserScoresQueryVariables,
+  APITypes.SyncUserScoresQuery
+>;
