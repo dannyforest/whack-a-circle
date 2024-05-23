a41e01187b0b866e2b5fe5db200c616e7a881702
Albus9000 <157077551+Albus9000@users.noreply.github.com>
2024-05-23 14:20:37 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import ClickTarget from "./Components/ClickTarget";
-
+import { signOut } from '@aws-amplify/auth'
+import { DataStore } from '@aws-amplify/datastore'
+import { UserScore } from './models'
+import { getCurrentUser } from '@aws-amplify/auth'
 function App() {
     const [counter, setCounter] = useState(0);
+    const [name, setName] = useState("Test User");
     const [missCounter, setMissCounter] = useState(0);
+    const [userId, setUserId] = useState("") // Hardcoded user id for now, in a real app this would be the user's sub from cognito
+    useEffect(() => {
+        getCurrentUser().then(({userId}) => {
+            setUserId(userId);
+        })
+    }, []);
 
-    const handleClick = () => {
+    const handleClick = async () => {
         setCounter(counter + 1);
+    
+        if (!userId) return;
+    
+        const userScores  = await DataStore.query(UserScore, (c) => c.userId.eq(userId));
+        const original = userScores.length > 0 ? userScores[0] : null;
+        if (original) {
+            const updatedUserScore = await DataStore.save(
+                UserScore.copyOf(original, updated => {
+                    updated.score = counter + 1
+                })
+            );
+        } else {
+            await DataStore.save(new UserScore({userId: userId, name: name, score: counter + 1}));
+        }
     }
     const handleMiss = () => {
         setMissCounter(missCounter + 1);
     }
     return (
         <div className="App">
+            <button onClick={() => signOut()}>Sign out</button>
             <ClickTarget
                 timeToDisplay={1000}
                 timeToSwitch={1500}
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import config from "./aws-exports";
 import reportWebVitals from "./reportWebVitals";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+Amplify.configure(config);
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
