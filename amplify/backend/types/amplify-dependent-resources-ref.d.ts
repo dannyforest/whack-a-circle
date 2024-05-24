8ef90e840bccef9ab44da287be5f953bd3c0e69b
Benaissanabila <benaissa.nabila14@gmail.com>
2024-05-23 21:01:42 -0400

    update api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,22 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "whackacircle": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "whackacirclec179707f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "whackacircle": {
       "Arn": "string",
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,87 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import ClickTarget from "./Components/ClickTarget";
+import {signOut} from "@aws-amplify/auth";
+import { DataStore } from 'aws-amplify/datastore';
+import {UserScore} from "./models";
+import { getCurrentUser } from 'aws-amplify/auth';
+import styled from "styled-components";
 import GameButton from "./Components/GameButton";
+import Button from "./Components/Button";
 
 function App() {
     const [counter, setCounter] = useState(0);
+    const [highestScore, setHighestScore] = useState(0);
+    const [name, setName] = useState('Danny');
+    const [userId, setUserId] = useState<string | null>(null);
 
-    const handleClick = () => {
+    useEffect(() => {
+        getCurrentUser().then(({userId}) => {
+            setUserId(userId);
+            loadAndSetHighestScore(userId).then();
+        })
+    }, []);
+
+    const getLastUserScore = async (userId: string): Promise<UserScore | null> => {
+        const userScores  = await DataStore.query(UserScore, (c) => c.userId.eq(userId));
+        return userScores.length > 0 ? userScores[userScores.length - 1] : null;
+    }
+
+    const loadAndSetHighestScore = async (userId: string) => {
+        const original = await getLastUserScore(userId);
+        if (original) {
+            setHighestScore(original.score);
+        }
+    }
+
+    const handleClick = async () => {
         setCounter(counter + 1);
+        if (counter + 1 > highestScore) {
+            setHighestScore(counter + 1);
+        }
+
+        if (!userId) return;
+
+        const original = await getLastUserScore(userId);
+        if (original && original.score > highestScore) {
+            await DataStore.save(
+                UserScore.copyOf(original, updated => {
+                    updated.score = counter + 1
+                })
+            );
+        } else {
+            await DataStore.save(new UserScore({userId: userId, name: name, score: counter + 1}));
+        }
+    }
+
+    const resetCounter = () => {
+        setCounter(0);
     }
     const handleReset=()=>{
         setCounter(0);
-
     }
     return (
         <div className="App">
+            <BackDiv
+                onClick={() => handleReset()}
+            />
             <ClickTarget
                 timeToDisplay={1000}
                 timeToSwitch={1500}
-                handleClick={() => handleClick()} />
-            <GameButton handleReset={() =>  handleReset()} />
+                resetCounter ={() => handleReset()}
+                handleClick={() => handleClick()}/>
+            <GameButton handleReset={() => handleReset()}/>
+            <Button onClick={async () => await signOut()}>Sign out</Button>
+            <p>Current Score: {counter}</p>
+            <p>Highest Score: {highestScore}</p>
             <p>{counter}</p>
         </div>
     );
 }
 
 export default App;
+const BackDiv = styled.div`
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+`;
